455d18dd93198cce539c7dd4beee5439ac939441
Dkinger <danielvkinger@gmail.com>
2023-04-07 20:22:04 -0500

    Update pins_arduino.h

--- variants/rpipico/pins_arduino.h
+++ variants/rpipico/pins_arduino.h
@@ -8,17 +8,15 @@
 #define PIN_LED        (25u)
 
 // Serial
-#define PIN_SERIAL1_TX (0u)
-#define PIN_SERIAL1_RX (1u)
 
 #define PIN_SERIAL2_TX (8u)
 #define PIN_SERIAL2_RX (9u)
 
 // SPI
-#define PIN_SPI0_MISO  (16u)
-#define PIN_SPI0_MOSI  (19u)
-#define PIN_SPI0_SCK   (18u)
-#define PIN_SPI0_SS    (17u)
+#define PIN_SPI0_MISO  (0u)
+#define PIN_SPI0_MOSI  (3u)
+#define PIN_SPI0_SCK   (2u)
+#define PIN_SPI0_SS    (1u)
 
 #define PIN_SPI1_MISO  (12u)
 #define PIN_SPI1_MOSI  (15u)
